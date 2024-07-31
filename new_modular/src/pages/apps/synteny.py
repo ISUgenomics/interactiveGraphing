98cc39b6b40357837d42dadd25c3f0f9b4111247
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-07-30 20:11:10 -0500

    due to changes in the main layout, specific app layout is temporarily disabled;

--- new_modular/src/pages/apps/synteny.py
+++ new_modular/src/pages/apps/synteny.py
@@ -1,30 +1,27 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-from src.options.uploads import opts_inputs
-from src.options.analysis import opts_analysis
-from src.options.graph_general import opts_graph
-from src.options.export import opts_config
-from src.layout.options import left_panel
-from src.layout.graphing import right_panel
+#from src.options.uploads import opts_inputs
+#from src.options.analysis import opts_analysis
+#from src.options.graph_general import opts_graph
+#from src.options.export import opts_config
 
 # OPTIONS Components assembly
 
 # Define the specific accordion items for the Synteny app
-accordion_items = [
-    dbc.AccordionItem(opts_inputs, title="1. UPLOAD INPUTS", item_id="item-1"),
-    dbc.AccordionItem(opts_analysis, title="2. ADJUST ANALYSIS SETTINGS", item_id="item-2"),
-    dbc.AccordionItem(opts_graph, title="3. GENERAL GRAPH SETTINGS", item_id="item-3"),
-    dbc.AccordionItem(html.Div(), title="4. CUSTOMIZE SYNTENY PLOT", item_id="item-4"),
-    dbc.AccordionItem(opts_config, title="5. EXPORT GRAPH", item_id="item-7", class_name=".container")
-]
+#accordion_items = [
+#    dbc.AccordionItem(opts_inputs, title="1. UPLOAD INPUTS", item_id="item-1"),
+#    dbc.AccordionItem(opts_analysis, title="2. ADJUST ANALYSIS SETTINGS", item_id="item-2"),
+#    dbc.AccordionItem(opts_graph, title="3. GENERAL GRAPH SETTINGS", item_id="item-3"),
+#    dbc.AccordionItem(html.Div(), title="4. CUSTOMIZE SYNTENY PLOT", item_id="item-4"),
+#    dbc.AccordionItem(opts_config, title="5. EXPORT GRAPH", item_id="item-7", class_name=".container")
+#]
 
 # Add the accordion to the optionsDiv
-left_panel.children[1].children.append(
-    dbc.Accordion(accordion_items, id="accordion", start_collapsed=True, always_open=True, flush=False)
-)
+#left_panel.children[1].children.insert(-1,
+#    dbc.Accordion(accordion_items, id="accordionA", start_collapsed=True, always_open=True, flush=False)
+#)
 
 # Set up the app layout
 layout = html.Div([
-    left_panel,
-    right_panel
+
 ], id='app', className="d-flex", style={'width':'100%', 'height':'100%', 'overflow-y':'hidden'})
