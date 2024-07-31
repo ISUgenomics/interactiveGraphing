abb66d5b8590b76864398a1d22d446386616c8e3
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-07-31 17:41:26 -0500

    fixed id of the show-options button; adjusted style attributes to dash formatting;

--- new_modular/src/layout/options.py
+++ new_modular/src/layout/options.py
@@ -8,7 +8,7 @@ from src.options.graph_custom import opts_graph_types
 from src.options.export import opts_config
 
 left_panel = html.Div([
-    html.Button('Show Options', id='btn-options', n_clicks=0, style=css_btn),
+    html.Button('Show Options', id='options', n_clicks=0, style=css_btn),
     html.Div(id='optionsDiv', children=[
         dbc.Accordion([
             dbc.AccordionItem(opts_inputs, title="1. UPLOAD INPUTS", item_id="item-1"),
@@ -16,7 +16,7 @@ left_panel = html.Div([
             dbc.AccordionItem(opts_graph, title="3. GENERAL GRAPH SETTINGS", item_id="item-3"),
             dbc.AccordionItem(opts_graph_types, title="4. SPECIFIC GRAPH SETTINGS", item_id="item-4"),
             dbc.AccordionItem(opts_config, title="5. EXPORT GRAPH", item_id="item-5", class_name=".container")            
-        ], id="accordion", always_open=True, flush=False, active_item="item-4"), #start_collapsed=True,
+        ], id="accordion", always_open=True, flush=False, start_collapsed=True), #,active_item="item-4"
         html.Div(id="accordion-contents", className="mt-3"),
-    ], style={'margin-right': '1px', 'width': '100%'})
+    ], style={'marginRight': '1px', 'width': '100%'})
 ], id='left-panelDiv', style=css_lpd)
