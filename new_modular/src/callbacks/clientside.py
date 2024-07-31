e7881ee08280d2918c71cc4b56d5bec101102428
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-07-31 18:02:56 -0500

    toggleOptionsPanel() and unfoldEditPanel() moved to JS file; tested toggleOptionsPanel() and moveDataTableButtons(); detected JS issues related to complex dash ids in unfoldEditPanel() and createItemButtons() - need further debugging (check docs for latest dash release used in this approach);

--- new_modular/src/callbacks/clientside.py
+++ new_modular/src/callbacks/clientside.py
@@ -2,38 +2,41 @@ from dash import ClientsideFunction
 from dash.dependencies import Input, Output, State, ALL
 
 def register_clientside_callbacks(app):
+    
     # toggle the display of the options panel on the left
     app.clientside_callback(
         """
         function(n_clicks) {
-            window.dash_clientside.optionsPanel.toggleOptions(n_clicks);
-            return {};
+            return window.dash_clientside.clientside.toggleOptionsPanel(n_clicks);
         }
         """,
         Output('optionsDiv', 'style'),
-        Input('btn-options', 'n_clicks')
-    )
-    # automatically unfold the Edit Input Data panel when a file is selected for editing
-    app.clientside_callback(
-        """
-        function(n_clicks, activeItems) {
-            return window.dash_clientside.rightPanel.unfoldEditPanel(n_clicks, activeItems);
-        }
-        """,
-        Output('accordion2', 'active_item'),
-        [Input({'type': 'edit-', 'id': ALL}, 'n_clicks')],
-        [State('accordion2', 'active_item')],
+        Input('options', 'n_clicks'),
         prevent_initial_call=True
     )
+
+#    app.clientside_callback(
+#        """
+#        function(n_clicks, activeItems) {
+#            return window.dash_clientside.clientside.unfoldEditPanel(n_clicks, activeItems);
+#        }
+#        """,
+#        Output('accordion2', 'active_item'),
+#        [Input({'type': 'edit-', 'id': ALL}, 'n_clicks')],
+#        [State('accordion2', 'active_item')],
+#        prevent_initial_call=True
+#    )
+
     # create item buttons for DataTables
-    app.clientside_callback(
-        ClientsideFunction(
-            namespace='rightPanel',
-            function_name='createItemButtons'
-        ),
-        Output('void1', 'value'),
-        [Input({'type': 'item-', 'id': ALL}, 'n_clicks')]
-    )
+#    app.clientside_callback(
+#        ClientsideFunction(
+#            namespace='rightPanel',
+#            function_name='createItemButtons'
+#        ),
+#        Output('void1', 'value'),
+#        [Input({'type': 'item-', 'id': ALL}, 'n_clicks')]
+#    )
+    
     # move DataTable buttons
     app.clientside_callback(
         ClientsideFunction(
