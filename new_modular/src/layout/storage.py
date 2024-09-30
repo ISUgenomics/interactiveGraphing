4a5b3330194291a7e750a61b5d45bc923f1617d6
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-09-30 11:09:18 -0500

    added store for graph settings and graph data

--- new_modular/src/layout/storage.py
+++ new_modular/src/layout/storage.py
@@ -11,9 +11,11 @@ storage = html.Div([
   dcc.Store(id="edition-content", data={}, storage_type='session'),      # real content of input edition section (all data tables): initial dataframe, used for reset
   dcc.Store(id="edition-current", data={}, storage_type='session'),      # real content of input edition section (all data tables): current state, used for onload
   dcc.Store(id="edition-history", data={}, storage_type='session'),      # trace of changes made in (all data tables): changes only, used for undo
-#  dcc.Store(id="graph-data", data={}, storage_type='session'),
+  dcc.Store(id='captured-name-store', data='', storage_type='session'),
   dcc.Store(id="graph-config", data=CONFIG, storage_type='session'),     # current config of built-in interactive tools in the graph
-    
+#  dcc.Store(id="graph-options-store", data={}, storage_type='session'),  # dict with keys of current graph tabs with values of all options for a given graph
+  dcc.Store(id="graph-data", data={}, storage_type='session'),           # dict that stores graph data for each plotting tab; tab-id : {subset : df_data}
+
   html.Button(id="edition-undo", children='', name="True", hidden=True, n_clicks=0)         # object that stores info for undo action
 ])
 
@@ -21,6 +23,10 @@ void = html.Div([
   dcc.Input(id="void0", value='', type='hidden'),    # html export
   dcc.Input(id="void1", value='', type='hidden'),    # clientside JS: CreateCloseButtons for DTs
   dcc.Input(id="void2", value='', type='hidden'),    # clientside JS: add tooltip when selecting rows
+])
+
+dummy = html.Div ([
+  
 ])
 
 identifiers = html.Div([
