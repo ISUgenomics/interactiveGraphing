87e33c281e5e83b62f602594829399bf5ea086b7
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-09-30 11:08:03 -0500

    added id of the layout component

--- new_modular/src/options/analysis.py
+++ new_modular/src/options/analysis.py
@@ -1,6 +1,14 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
 ## 2. ADJUST ANALYSIS SETTINGS
 
-
+synteny = [
+    html.Label('select graph inputs: ', className='mb-1 label-s'),
+    dcc.Dropdown(id="synteny-inputs", persistence=True, persistence_type="session"),
+    html.Label('select genomes: ', className='mb-1 mt-3 label-s'),
+    dcc.Dropdown(id="synteny-genomes", multi=True, persistence=True, persistence_type="session"),
+]
 
 
 
@@ -11,6 +19,4 @@
 
 
 # Final assembly of analysis options
-opts_analysis = [
-
-]
+opts_analysis = html.Div(id="opts-analysis")
