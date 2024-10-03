93b68fdd98ae0e8960c6f87cbbf76efa01d2c318
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-10-03 09:55:44 -0500

    added store and layout components for dynamic chromosome selection in the synteny app options panel

--- new_modular/src/options/analysis.py
+++ new_modular/src/options/analysis.py
@@ -12,6 +12,8 @@ synteny = [
     html.Label('select and order genomes: ', className='mb-1 mt-3 label-s'),
     dcc.Store(id="synteny-genomes-all", data=[], storage_type='session'),
     dcc.Store(id='synteny-genomes-selected', data=[], storage_type='session'),
+    dcc.Store(id='synteny-chromosomes-all', data={}, storage_type='session'),
+    dcc.Store(id='synteny-chr-selected', data=[], storage_type='session'),
     # Draggable GridLayout for genome selection
     dash_draggable.GridLayout(id='genome-draggable', children=[], 
         gridCols=2, width=300, height=28, className="w-100 mt-0 pt-0 shift-up"
@@ -19,7 +21,7 @@ synteny = [
 
     html.Label('select and order chromosomes: ', className='mb-1 label-s'),
     dcc.RadioItems(['order', 'min-dist','manual'], value='order', inline=True, labelClassName="ms-0 me-3"),
-
+    html.Div(id="synteny-chromosome-selection"),
 ]
 
 
