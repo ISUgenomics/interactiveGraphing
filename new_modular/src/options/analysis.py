78b529a1667db11a406518320cef3a4bdcf9fd26
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-10-07 11:42:09 -0500

    moved synteny-related storage components

--- new_modular/src/options/analysis.py
+++ new_modular/src/options/analysis.py
@@ -10,10 +10,6 @@ synteny = [
     dcc.Dropdown(id="synteny-inputs", persistence=True, persistence_type="session"),
 
     html.Label('select and order genomes: ', className='mb-1 mt-3 label-s'),
-    dcc.Store(id="synteny-genomes-all", data=[], storage_type='session'),
-    dcc.Store(id='synteny-genomes-selected', data=[], storage_type='session'),
-    dcc.Store(id='synteny-chromosomes-all', data={}, storage_type='session'),
-    dcc.Store(id='synteny-chr-selected', data=[], storage_type='session'),
     # Draggable GridLayout for genome selection
     dash_draggable.GridLayout(id='genome-draggable', children=[], 
         gridCols=2, width=300, height=28, className="w-100 mt-0 pt-0 shift-up"
