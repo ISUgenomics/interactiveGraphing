df9b2fd93977e2712d50574173f9482cbf6f6758
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-10-22 18:22:51 -0500

    updated callbacks to persist also graphs and output-data sections in the viewer panel; updated Inputs/Outputs/States with pattern-matching ids

--- new_modular/src/callbacks/basic.py
+++ new_modular/src/callbacks/basic.py
@@ -3,24 +3,27 @@ from dash import dcc, html, no_update, callback_context
 from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State, ALL
 from src.layout.options import create_left_panel
-from src.pages.config import visualizations
-from src.pages.apps import synteny, clustergram
 
 
 def register_basic_callbacks(app):
 
     # Add a new app-tab based on the selected analysis/graphing (at Home tab); make Tabs persistent using storage solution;
-    @app.callback([Output('tabs', 'children'), Output('add-app-tab-num', 'data'), Output('optionsDiv', 'children'), Output('left-panel-children', 'data')],
+    @app.callback([Output('tabs', 'children'), Output('add-app-tab-num', 'data'),                       # tabs & storage
+                   Output('optionsDiv', 'children'), Output('left-panel-children', 'data'),             # options & storage
+                   Output('graph-panelDiv', 'children'), Output('graph-panel-children', 'data'),        # graphs & storage
+                   Output('lower-panelDiv', 'children'), Output('output-panel-children', 'data')],      # outputs & storage
                    Input('add-app-tab', 'n_clicks'),
-                  [State('tabs', 'children'), State('visualizations', 'value'), State('add-app-tab-num', 'data'), State('left-panel-children', 'data')]
+                  [State('tabs', 'children'), State('visualizations', 'value'), State('add-app-tab-num', 'data'), 
+                   State('left-panel-children', 'data'), State('graph-panel-children', 'data'), State('output-panel-children', 'data')],
+                   prevent_initial_call = True
     )
-    def manage_app_tabs(n_clicks, existing_tabs, selected_app, app_tabs, left_content):
+    def manage_app_tabs(n_clicks, existing_tabs, selected_app, app_tabs, left_content, graph_content, output_content):
 #        print(n_clicks, len(existing_tabs), len(app_tabs), existing_tabs[0])                                                          ######### DEBUG
         if n_clicks == 0:
-            if not len(app_tabs):
-                return [no_update, existing_tabs, left_content, left_content]
-            elif len(app_tabs) > len(existing_tabs):
-                return [app_tabs, no_update, left_content, left_content]
+            if not len(app_tabs):                           # if tabs register empty, create it based on existing tabs
+                return [no_update, existing_tabs, no_update, no_update, no_update, no_update, no_update, no_update]
+            elif len(app_tabs) > len(existing_tabs):        # on page reload, display all tabs
+                return [app_tabs, no_update, no_update, no_update, no_update, no_update, no_update, no_update]
             else:
                 raise PreventUpdate
         if len(app_tabs) >= 10:                                                                                                      # UPGRADE: (optional) use 'disable_n_clicks' to prevent creating more app-tabs than n=10
@@ -39,28 +42,32 @@ def register_basic_callbacks(app):
         num = found_value + 1 if found_value else 1
 
         tab_name = f"{selected_app}_{num}"
-        new_tab = dbc.Tab(label=tab_name, id={'type':"app-tab-",'id': tab_name}, tab_id=f"tab-{tab_name}", class_name='index-tab')
+        new_tab = dbc.Tab(label=tab_name, id={'id':"app-tab-",'tab': tab_name}, tab_id=f"tab-{tab_name}", class_name='index-tab')
         existing_tabs.append(new_tab)
 
-        left_content.append(html.Div(children=create_left_panel(tab_name), id={'type':"lopts-",'id': tab_name}, className='d-none'))
+        left_content.append(html.Div(children=create_left_panel(tab_name), id={'id':"lopts-",'tab': tab_name}, className='d-none'))
+        graph_content.append(html.Div(children=[dcc.Graph(id={'id': "graph", 'tab': tab_name})], id={'id':"graphing-",'tab': tab_name}, className='d-none'))
+        output_content.append(html.Div(id={'id': "outputs-", 'tab': tab_name}, className='d-none'))
 
-        return [existing_tabs, existing_tabs, left_content, left_content]
+        return [existing_tabs, existing_tabs, left_content, left_content, graph_content, graph_content, output_content, output_content]
 
 
     # Manage the visibility of options for a currently viewed graphing app; also display/hide the entire app-mode view
-    @app.callback([Output({'type': 'lopts-', 'id': ALL}, 'className'), Output('app-mode', 'className')],
-                  Input('tabs', 'active_tab'),
-                 [State({'type': 'lopts-', 'id': ALL}, 'id')]
+    @app.callback([Output({'id': 'lopts-', 'tab': ALL}, 'className'), Output({'id': 'graphing-', 'tab': ALL}, 'className'), Output({'id': 'outputs-', 'tab': ALL}, 'className'), 
+                   Output('app-mode', 'className')],
+                   Input('tabs', 'active_tab'),
+                  [State({'id': 'lopts-', 'tab': ALL}, 'id')],
+                   prevent_initial_call = True
     )
     def manage_opts_display(active_tab, items):
         if not callback_context.triggered:
             raise PreventUpdate
         if active_tab == 'tab-home' or active_tab == 'tab-about':
-            return [[no_update] * len(items), 'd-none']
+            return [[no_update] * len(items), [no_update] * len(items), [no_update] * len(items), 'd-none']
         else:
             tab_name = active_tab.split('-')[-1]
-            hidden = ['d-none' if item['id'] != tab_name else 'd-block' for item in items]
-            return [hidden, 'd-flex']
+            show_active = ['d-none' if item['tab'] != tab_name else 'd-block' for item in items]
+            return [show_active, show_active, show_active, 'd-flex']
 
 
 
