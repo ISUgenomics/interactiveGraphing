4232764a984a0462ab3f80cba6496d6fd1c2a443
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-10-22 18:05:22 -0500

    updated a register

--- new_modular/src/pages/__init__.py
+++ new_modular/src/pages/__init__.py
@@ -1,5 +1,4 @@
 from .index import layout as index_layout
 from .about import layout as about_layout
-from .apps.synteny import layout as synteny_layout
 
-__all__ = ['index_layout', 'about_layout', 'synteny_layout']
+__all__ = ['index_layout', 'about_layout']
