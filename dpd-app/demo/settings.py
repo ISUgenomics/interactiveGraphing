b8a17f796cfd1be68cb2ac8f633ccd1cef637604
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2024-11-08 14:01:34 -0600

    IMPORTANT: disabled insertion of demo examples (they wont be added into model database); set serve assets locally for all apps;

--- dpd-app/demo/settings.py
+++ dpd-app/demo/settings.py
@@ -133,19 +133,15 @@ USE_TZ = True
 
 PLOTLY_DASH = {
     "ws_route" : "ws/channel",
-
-    "insert_demo_migrations" : True,  # Insert model instances used by the demo
-
+    "http_route" : "dpd/views",
+    "insert_demo_migrations" : False,  # Insert model instances used by the demo
     "http_poke_enabled" : True, # Flag controlling availability of direct-to-messaging http endpoint
-
     "view_decorator" : None, # Specify a function to be used to wrap each of the dpd view functions
-
     "cache_arguments" : True, # True for cache, False for session-based argument propagation
-
-    #"serve_locally" : True, # True to serve assets locally, False to use their unadulterated urls (eg a CDN)
-
+    "serve_locally" : True, # True to serve assets locally, False to use their unadulterated urls (eg a CDN)
     "stateless_loader" : "demo.views.stateless_app_loader",
     }
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
